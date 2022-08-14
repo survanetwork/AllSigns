b4bfd39141e2acb3cd0479ecf06c32566bb94bd0
Jarne <dev@jarne.rocks>
2022-08-14 18:17:08 +0200

    Updated translation message method to new format

--- src/surva/allsigns/AllSigns.php
+++ src/surva/allsigns/AllSigns.php
@@ -182,7 +182,9 @@ class AllSigns extends PluginBase
      */
     public function getMessage(string $key, array $replaces = []): string
     {
-        if (($rawMessage = $this->messages->getNested($key)) === null) {
+        $rawMessage = $this->messages->getNested($key);
+
+        if ($rawMessage === null || $rawMessage === "") {
             $rawMessage = $this->defaultMessages->getNested($key);
         }
 
